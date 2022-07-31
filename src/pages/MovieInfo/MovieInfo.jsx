46905be65c17efe2761cfac320eeff135cdcc3ad
Preksler <preksler@gmail.com>
2022-08-01 01:05:28 +0300

    fix goBack Link

--- src/pages/MovieInfo/MovieInfo.jsx
+++ src/pages/MovieInfo/MovieInfo.jsx
@@ -61,8 +61,8 @@ const MovieInfo = () => {
             <Wrapper>
               <AdditionalText>Additional information</AdditionalText>
               <AdditionalList>
-                <AdditionalLink to="cast">Cast</AdditionalLink>
-                <AdditionalLink to="reviews">Reviews</AdditionalLink>
+                <AdditionalLink to="cast" state={{from: location.state.from}}>Cast</AdditionalLink>
+                <AdditionalLink to="reviews" state={{from: location.state.from}}>Reviews</AdditionalLink>
               </AdditionalList>
             </Wrapper>
         </div>
